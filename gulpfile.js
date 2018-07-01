ddebc8021ea8e918ca5251e2f128c9782821308a
Elena Karpenko <karp.ol3000@gmail.com>
2018-07-01 18:58:11 +0300

    half-done

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass'); 
-// const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scss', function () {
-    return gulp.src('./src/scss/pages/**/*.scss')
+    return gulp.src('src/scss/pages/*.scss')
         
-        .pipe(sass().on('error', sass.logError))
-        // .pipe(autoprefixer({
-        //     browsers: ['last 2 versions'],
-        //     cascade: false
-        // }))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('scss:watch', function () {
-    gulp.watch('./src/scss/**/*.scss', ['scss']);
+gulp.task('watch', function () {
+    gulp.watch('src/scss/pages/*.scss', ['scss']);
 });
