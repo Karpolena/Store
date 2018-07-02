890f3e0e5e156375c4a0efc9f431ff49b48a8b40
Elena Karpenko <karp.ol3000@gmail.com>
2018-07-02 14:46:22 +0300

    ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass'); 
-const autoprefixer = require('gulp-autoprefixer');
+// const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scss', function () {
     return gulp.src('src/scss/pages/*.scss')
         .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
+        // .pipe(autoprefixer({
+        //     browsers: ['last 2 versions'],
+        //     cascade: false
+        // }))
         .pipe(gulp.dest('public/css'));
 });
 
